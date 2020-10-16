ebb697d6af5e667e55fa9c07a7ac8584db985da6
faris <faris38kanbur@gmail.com>
2020-10-16 18:11:58 +0200

    feat:added new code and pic of app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {StyleSheet, View, Text, FlatList} from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -11,15 +12,22 @@ export default function App() {
   ]);
 
   const pressHandler = (key) => {
-    setTodos((pressTodos) => {
-      return pressTodos.filter((todo) => todo.key != key);
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
     });
   };
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) =>{
+      return [{text: text, key: Math.random().toString()}, ...prevTodos];
+    });
+  }
 
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
